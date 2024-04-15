b7678547361cc6ff8b425f90f8cfba60b289f299
ashuri17 <150310192+ashuri17@users.noreply.github.com>
2024-04-15 15:34:44 +0800

    removed redundant printLn in question3

--- Question3.java
+++ Question3.java
@@ -5,7 +5,6 @@ public class Question3 {
   public static void main(String[] args) {
 
     Scanner in = new Scanner(System.in);
-    System.out.print("Enter an integer: ");
     int num = in.nextInt();
     int result = num * num;
     System.out.println("The result is " + result);
